14c18547abbdc6f55808349fa7bee541189a6e09
4zzi <kaze60613@gmail.com>
2026-08-11 21:19:24 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
 @echo off
-wt -d "D:\aurora discord username gen" cmd /k "title - AUR I Username Generator - && python gen.py"
+wt -d "location to folder" cmd /k "title - AUR I Username Generator - && python gen.py"
